cefd99dfee1efe225aa2eb4cea3b7d65f6daddc1
Vasudevan <vasu3797@gmail.com>
2019-01-29 17:47:09 +0530

    changed html

--- flasking.py
+++ flasking.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
-import datetime
 
-app  = Flask(__name__)
 
-@app.route("/<string:name>")
-def hello(name):
-    now = datetime.datetime.now()
-    is_new_years = now.month == 1 and now.day == 1
-    return render_template("index,html", is_new_years=is_new_years)
+app =  Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/more")
+def more():
+    return render_template("more.html")
